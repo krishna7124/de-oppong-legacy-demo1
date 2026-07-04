ebc2d503d52d51791719583410effcb2df352f37
Krishna <krishnabhatt863@gmail.com>
2026-07-04 16:18:25 +0530

    fix: favicon.ico in public + static link in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 import "./styles.css";
-import faviconUrl from "./assets/favicon.ico";
-
-const link = document.createElement("link");
-link.rel = "icon";
-link.type = "image/x-icon";
-link.href = faviconUrl;
-document.head.appendChild(link);
 
 const router = getRouter();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
